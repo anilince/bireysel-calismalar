eb1190d493aebc848d024ac3b06fa054c5ec26b4
Anıl İnce <100514281+anilince@users.noreply.github.com>
2022-06-16 14:27:39 +0300

    Add files via upload

---
+++ ebob-bulma/app.js
@@ -0,0 +1,17 @@
+let sayi1 = parseInt(prompt("Birinci sayıyı giriniz"));
+let sayi2 = parseInt(prompt("İkinci sayıyı giriniz"));
+
+let ebob = 1, kontrol = 2;
+
+while (kontrol <= sayi1 && kontrol <= sayi2) {
+    if (sayi1 % kontrol == 0 && sayi2 % kontrol == 0) {
+        ebob = kontrol;
+    }
+    kontrol++;
+}
+
+if (ebob == 1) {
+    console.log("Bu sayılar aralarında asaldır");
+} else {
+    console.log("Bu sayıların ortak böleni :" + ebob);
+}
